2af3ac7208ce7e35098727c3b716a7a38170b691
Niels de Hoog <njdehoog@gmail.com>
2015-08-19 11:07:24 +0200

    Initial commit

---
+++ swift-sass/Sass.swift
@@ -0,0 +1,17 @@
+//
+//  Sass.swift
+//  swift-sass
+//
+//  Created by Niels de Hoog on 19/08/15.
+//  Copyright © 2015 Invisible Pixel. All rights reserved.
+//
+
+import Foundation
+
+
+struct Sass {
+    static func compile(scss: String) -> String {
+        
+        return ""
+    }
+}
---
+++ swift-sass/SwiftSass.h
@@ -0,0 +1,19 @@
+//
+//  swift-sass.h
+//  swift-sass
+//
+//  Created by Niels de Hoog on 19/08/15.
+//  Copyright © 2015 Invisible Pixel. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for swift-sass.
+FOUNDATION_EXPORT double swift_sassVersionNumber;
+
+//! Project version string for swift-sass.
+FOUNDATION_EXPORT const unsigned char swift_sassVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swift_sass/PublicHeader.h>
+
+
